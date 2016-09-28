1c83fe69c45c8de1eebb8eb47c10fe3dccefe516
Melvin Walls <mwalls67@gmail.com>
2016-09-28 10:40:50 -0700

    reorganize things

---
+++ src/aper/mod.rs
@@ -0,0 +1,30 @@
+mod decoder;
+pub use self::decoder::{Decoder, DecodeError};
+
+#[derive(PartialEq)]
+pub enum ConstraintFlags {
+    UNCONSTRAINED = 0x0,
+    SEMI_CONSTRAINED = 0x1,
+    CONSTRAINED = 0x2,
+    EXTENSIBLE = 0x3,
+}
+
+pub struct Constraint {
+    flags: ConstraintFlags,
+    range_bits: u32,
+    effective_bits: u32,
+    min: i64,
+    max: i64,
+}
+
+pub struct Constraints {
+    value: Constraint,
+    size: Constraint,
+}
+
+pub trait APerElement {
+    type Result;
+    const TAG: u32;
+    const CONSTRAINTS: Option<Constraints>; // visible constraints
+    fn decode(data: &[u8]) -> Result<Self::Result, decoder::DecodeError>;
+}
