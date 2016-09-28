6828eb5f2aee7c32543b011f081139b72616992b
Melvin Walls <mwalls67@gmail.com>
2016-09-28 10:28:42 -0700

    add some basic int tests

---
+++ tests/integers.rs
@@ -0,0 +1,24 @@
+extern crate asn1;
+use asn1::aper;
+
+// XXX: negative value tests
+#[test]
+fn unconstrained_bounds() {
+    let data = b"\x02\x10\x00";
+    let mut d = aper::Decoder::new(data);
+    assert_eq!(4096, d.decode_int(None, None).unwrap());
+}
+
+#[test]
+fn constrained_bounds() {
+    let data = b"\x00";
+    let mut d = aper::Decoder::new(data);
+    assert_eq!(Ok(4000), d.decode_int(Some(4000), Some(4255)));
+}
+
+#[test]
+fn semiconstrainted_bounds() {
+    let data = b"\x02\x10\x01";
+    let mut d = aper::Decoder::new(data);
+    assert_eq!(Ok(4096), d.decode_int(Some(-1), None));
+}
