63955ddeb16f3dfec85ec1132fc2c8886ce6abd5
Melvin Walls <mwalls67@gmail.com>
2016-09-28 13:55:47 -0700

    add unpadded integer tests

--- tests/integers.rs
+++ tests/integers.rs
@@ -17,6 +17,14 @@ fn constrained_bounds() {
 }
 
 #[test]
+fn constrained_bounds_unpadded() {
+    let data = b"\x06";
+    let mut d = aper::Decoder::new(data);
+    assert_eq!(Ok(10), d.decode_int(Some(10), Some(12)));
+    assert_eq!(Ok(11), d.decode_int(Some(10), Some(12)));
+}
+
+#[test]
 fn semiconstrainted_bounds() {
     let data = b"\x02\x10\x01";
     let mut d = aper::Decoder::new(data);
