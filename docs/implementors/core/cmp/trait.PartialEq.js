45454cff5a6042cd978c4b54fec2252285f16b56
Melvin Walls <mwalls67@gmail.com>
2016-10-08 00:55:15 -0700

    update docs

--- docs/implementors/core/cmp/trait.PartialEq.js
+++ docs/implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["asn1"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='asn1/aper/enum.DecodeError.html' title='asn1::aper::DecodeError'>DecodeError</a>",];
+implementors["asn1"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='asn1/aper/enum.DecodeError.html' title='asn1::aper::DecodeError'>DecodeError</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='asn1/aper/enum.EncodeError.html' title='asn1::aper::EncodeError'>EncodeError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
